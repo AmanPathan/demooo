e980a683ee2c6bea44db7c17ff861d4fd3766898
AmanPathan <amanpathan526@gmail.com>
2024-07-14 23:21:43 +0530

    Added code in master

--- demo.cpp
+++ demo.cpp
@@ -3,13 +3,6 @@ using namespace std;
 class A{
     public:
     static int a;
-    static void fun(){
-        cout<<"Class A Method\n";
-    }
-    void run(){
-        cout<<"Run";
-    }
-
 };
 int A::a = 6;
 int main(){
