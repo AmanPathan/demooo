e508ccf1a786f50bb18e812d60869a3d9ba2cf1e
AmanPathan <amanpathan526@gmail.com>
2024-07-14 14:19:21 +0530

    added code

---
+++ demo.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class A{
+    public:
+    static int a;
+    static void fun(){
+        cout<<"Class A Method\n";
+    }
+
+};
+int A::a = 6;
+int main(){
+    cout<<A::a;
+    A::fun();
+    return 0;
+}
